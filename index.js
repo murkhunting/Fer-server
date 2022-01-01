d5dbf7d63c734c60b2f84843af21e78f7435ee3f
Murkhunting <marcmp.arq@gmail.com>
2022-01-01 12:34:01 +0100

    conexion with database

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection SUCCESSFULL!"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
