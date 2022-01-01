7ff83b3827eb859a18c9ca6b16d6ebc853936aaf
Murkhunting <marcmp.arq@gmail.com>
2022-01-01 21:36:21 +0100

    gitignore

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //CREATE USER - POST
 router.post("/newuser", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
-    password: req.body.password,
-  });
+    password: Crypto.arguments.encrypt(
+        req.body.password,
+        process.env.
+  )});
   try {
     const user = await newUser.save();
     res.status(201).json(user);
