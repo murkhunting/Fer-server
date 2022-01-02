3a5da2e2f16b9a00e85bdf32531d5c1ec399fa79
Murkhunting <marcmp.arq@gmail.com>
2022-01-02 11:20:24 +0100

    login route encrypted

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,32 @@ const CryptoJS = require("crypto-js");
 router.post("/newuser", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
-    password: Crypto.arguments.encrypt(
-        req.body.password,
-        process.env.
-  )});
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
   try {
     const user = await newUser.save();
     res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN - POST
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong username or password");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong username or password");
+
+    res.status(200).json(user._doc);
   } catch (err) {
     res.status(500).json(err);
   }
